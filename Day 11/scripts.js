94f89002aa4b8faf546ac92a24238ff58f8564c9
Matt <mddever@gmail.com>
2019-04-01 19:00:26 -0600

    started work on fullscreen button functionality

--- Day 11/scripts.js
+++ Day 11/scripts.js
@@ -6,24 +6,24 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
-
+const fullscreenButton = player.querySelector('.fullscreen');
 
 
 /// Build the functions ///
-function togglePlay() {
-    if(video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-}
+// function togglePlay() {
+//     if(video.paused) {
+//         video.play();
+//     } else {
+//         video.pause();
+//     }
+// }
 /// can also be written as terniary
-/// function togglePlay() {
-/// const method = video.paused ? 'play' : 'pause';
-/// video[method]();} -- call the method on the video
+ function togglePlay() {
+ const method = video.paused ? 'play' : 'pause';
+ video[method]();} //-- call the method on the video
 
 function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
+    const icon = this.paused ? '►' : '❚❚';
     video[method]();
     toggle.textContent = icon;
 }
@@ -46,7 +46,13 @@ function scrub(e) { /// click to move to desired time function
 const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
 }
+
+function fullscreen(e) {
+    const size = window
+}
 /// hook up event listenters.
+toggle.addEventListener('click', fullscreen)
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
@@ -61,5 +67,5 @@ let mousedown = false;
 
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-progress.addEventListener('mousedown', () => mousedown = true)
-progress.addEventListener('mouseup', () => mousedown = false)
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
